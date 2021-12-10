cc1697af3ae356d4d10137edfa7f8137afd32ea8
Your UserName <aratinaik151997@gmail.com>
2021-12-10 18:47:50 +0530

    Added Enqueue() methhod to insert data in queue

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -12,12 +12,18 @@ namespace StackAndQueue
         {
             Console.WriteLine("Welcome to Stack and Queue operations using Linked List");
             StackOperations stackList = new StackOperations();
-            stackList.Push(70);
-            stackList.Push(30);
-            stackList.Push(56);
-            stackList.Display();
-            stackList.IsEmpty();
-            stackList.Display();
+            //stackList.Push(70);
+            //stackList.Push(30);
+            //stackList.Push(56);
+            //stackList.Display();
+            //stackList.IsEmpty();
+            //stackList.Display();
+
+            QueueOperations queueList = new QueueOperations();
+            queueList.Enqueue(56);
+            queueList.Enqueue(30);
+            queueList.Enqueue(70);
+            queueList.Display();
             Console.ReadKey();
         }
     }
