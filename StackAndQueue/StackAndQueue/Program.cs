615a5079e6075dff0519b74fdb437490d79e5a9c
Your UserName <aratinaik151997@gmail.com>
2021-12-10 16:59:47 +0530

    Added push() method to add elements in stack

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -11,6 +11,11 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue operations using Linked List");
+            StackOperations stackList = new StackOperations();
+            stackList.Push(70);
+            stackList.Push(30);
+            stackList.Push(56);
+            stackList.Display();
             Console.ReadKey();
         }
     }
---
+++ StackAndQueue/StackAndQueue/StackOperations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+            
+    }
+    public class StackOperations
+    {
+        public Node top;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+            Console.WriteLine("Element {0} is added to stack", top.data);
+        }
+        public void Display()
+        {
+            if (top == null)
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                Node temp = top;
+                Console.Write("Elements in stack are :");
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data);
+                    temp = temp.next;
+                }
+            }
+               
+        }
+    }
+}
